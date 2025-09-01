a25cd035c7fbf44d93fcf65a0e8f1dc2b7e812e6
Igor Carvalho <igordankesttrout7@gmail.com>
2025-09-01 11:21:27 -0300

    feat: Implementação de card slider com animações suaves e layout responsivo

---
+++ script.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper(".wrapper", {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  //pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+
+  /* Responsive Breakpoints */
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  },
+});
